9409df3f8a0c3b873eb0fddb2d6c214f38cd7bc7
sabateur007 <aniketsingh0107@gmail.com>
2024-02-07 20:36:14 +0530

    Add project files.

---
+++ TradingEngine/TradingEngineServer.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TradingEngineServer.core.Configurations;
+
+
+namespace TradingEngineServer.core
+{
+    sealed class TradingEngineServer : BackgroundService, ITradingEngineServer
+    {
+        private readonly ILogger<TradingEngineServer> _logger;
+        private readonly TradingEngineServerConfigurations _configuration;
+        public TradingEngineServer(ILogger<TradingEngineServer> logger,IOptions<TradingEngineServerConfigurations> config) 
+        { 
+            _logger = logger?? throw new ArgumentNullException(nameof(logger));
+            _configuration = config.Value ?? throw new ArgumentNullException(nameof(config)) ;  // "??" is called nul coaleaase opertaor
+        }
+
+        public Task Run(CancellationToken token) => ExecuteAsync(token);
+        
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+           while(stoppingToken.IsCancellationRequested)
+            { }
+           return Task.CompletedTask;
+        }
+    }
+}
---
+++ TradingEngine/TradingEngineServerHostBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngineServer.core
+{
+    public sealed class TradingEngineServerHostBuilder
+    {
+        public static IHost BuildTradingEngineServer() => Host.CreateDefaultBuilder().ConfigureServices((Context, services) =>
+        {
+            //start with configurations
+
+
+        }).Build();
+    }
+}
